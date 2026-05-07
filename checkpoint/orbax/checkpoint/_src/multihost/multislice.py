Fix the "incompatible devices" error during single replica broadcast, under active mesh context
PiperOrigin-RevId: 912157930

checkpoint/orbax/checkpoint/_src/multihost/multislice.py
@@ -249,7 +249,9 @@ def _globalize_single_replica_arrays(
 
   if is_source:
     for s in inp.addressable_shards:
-      source_device_map[s.device] = jnp.expand_dims(s.data, axis=0)
+      sd_mesh = jax.sharding.Mesh(np.array([s.device]), ('_single',))
+      with sd_mesh:
+        source_device_map[s.device] = jnp.expand_dims(s.data, axis=0)
 
   device_buffers = []
   for d, index in global_sharding.addressable_devices_indices_map(
@@ -261,7 +263,9 @@ def _globalize_single_replica_arrays(
       # Use jax.numpy.zeros to allocate directly on device
       # to avoid Host RAM spike.
       slice_shape = _get_slice_shape(index, global_shape)
-      zero_data = jnp.zeros(slice_shape, dtype=inp.dtype, device=d)
+      sd_mesh = jax.sharding.Mesh(np.array([d]), ('_single',))
+      with sd_mesh:
+        zero_data = jnp.zeros(slice_shape, dtype=inp.dtype, device=d)
       device_buffers.append(zero_data)
 
   logging.vlog(